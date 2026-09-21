eb583d612dffd621cae4fea2e811655ba4222e7b
Juanlyk <je.lesmesc1@uniandes.edu.co>
2026-09-21 12:13:17 -0500

    Avances en new_map()

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,16 +1,27 @@
 import map_functions as mf
+import random
 
 def new_map (num_elements, load_factor, prime=109345121):
     
     mapa = {
         'prime': prime,
         'capacity': mf.next_prime(num_elements / load_factor),
-        'scale': ,
-        'shift': 0,
+        'scale': random.randint(1, prime - 1),
+        'shift': random.randint(0, prime - 1),
         'table': {
             'size': 0,
             'elements': [
                 
             ]
-        }
+        },
+        'current_factor': 0,
+        'limit_factor': load_factor,
+        'size': 0     
     }
+    
+    for i in range(mapa['capacity']):
+        diccionario = {
+            ''
+        }
+    
+    return mapa
