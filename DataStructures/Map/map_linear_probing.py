d1c6418752f0eb474cb59ab0440cfa6ba86f8939
Juanlyk <je.lesmesc1@uniandes.edu.co>
2026-09-21 15:41:04 -0500

    Implemenetación de las funciones size y new_map en map_linear_probing.py

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -3,27 +3,37 @@ import random
 
 def new_map (num_elements, load_factor, prime=109345121):
     
+    capacidad = mf.next_prime(num_elements / load_factor)
+    lista_elementos = []
+    
+    i = 0
+    while i < capacidad:
+        lista_elementos.append({
+            "key": None,
+            "value": None
+        })
+    
     mapa = {
         'prime': prime,
-        'capacity': mf.next_prime(num_elements / load_factor),
+        'capacity': capacidad,
         'scale': random.randint(1, prime - 1),
         'shift': random.randint(0, prime - 1),
         'table': {
             'size': 0,
-            'elements': [
-                
-            ]
+            'elements': lista_elementos
         },
         'current_factor': 0,
         'limit_factor': load_factor,
         'size': 0     
     }
     
-    for i in range(mapa['capacity']):
-        diccionario = {
-            ''
-        }
-    }
+    return mapa
+    
+def size (my_map):
+    
+    tamaño = my_map["size"]
+    
+    return tamaño
     
 def get(mapa, key):
     
